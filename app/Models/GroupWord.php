94fbfa757c192400325f241229ad7e795288dc40
abdelaziiiz <abdelazizhanzaz.jobs@gmail.com>
2021-02-14 18:55:14 +0100

    init first

---
+++ app/Models/GroupWord.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GroupWord extends Model
+{
+    use HasFactory;
+}
---
+++ database/factories/GroupWordFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\GroupWord;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GroupWordFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = GroupWord::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
